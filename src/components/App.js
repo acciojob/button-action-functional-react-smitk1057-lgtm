e6331b0e024568d0c05ca6056bfe1b6075eac018
Sumit Kumar <smitk1057@gmail.com>
2026-04-20 10:39:32 +0530

    ButtonClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [click,setClick]=useState()
+   function  handleClick(){
+    setClick('Hello, I ve learnt to use the full-stack evaluation tool. This makes me so happy')
+   }
+
   return (
     <div id="main">
-      // Do not alter the main div
+      
+     
+      <button  id ='click' onClick={handleClick}>Click</button>
+      <p id ='para'>{click}</p>
+      
     </div>
   );
 }
